143d8b4f3f8a0d877b4b676e4b97538e2aca036c
shamimbinnnur <sbnsbn00@gmail.com>
2022-10-13 23:34:48 +0600

    seperated 'star repo' from the drop-down menu

--- src/components/PrimaryNav.tsx
+++ src/components/PrimaryNav.tsx
@@ -52,129 +52,134 @@ const PrimaryNav = (): JSX.Element => {
         </div>
 
         {user && (
-          <Menu
-            as="div"
-            className="flex z-50 text-left relative"
-          >
-            <Menu.Button>
-              <div className="flex items-center">
-                <StarTheRepo />
+          <div className="flex">
+            <div className="hidden md:block">
+              <StarTheRepo />
+            </div>
 
-                <div className="hidden md:flex pl-4 border-l border-lightOrange">
-                  <div className="w-8 h-8 overflow-hidden rounded-full border-osOrange border">
-                    <img
-                      alt={String(user.user_metadata.user_name)}
-                      className="w-full h-full"
-                      src={getAvatarLink(String(user.user_metadata.user_name))}
-                    />
-                  </div>
-                </div>
-
-                <div className="flex md:hidden w-5 h-5">
-                  <GiHamburgerMenu size={24} />
-                </div>
-              </div>
-            </Menu.Button>
-
-            <Transition
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
+            <Menu
+              as="div"
+              className="flex z-50 text-left relative"
             >
-              <Menu.Items className="z-40 absolute right-0 top-10 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <Menu.Item>
-                  <div className="flex items-center px-2 py-2.5 mb-1 gap-x-2.5">
-                    <div className="flex-col shrink-0 grow-0 w-8 h-8 overflow-hidden rounded-full border-osOrange border">
+              <Menu.Button>
+                <div className="flex items-center">
+
+                  <div className="hidden md:flex pl-4 border-l border-lightOrange">
+                    <div className="w-8 h-8 overflow-hidden rounded-full border-osOrange border">
                       <img
                         alt={String(user.user_metadata.user_name)}
                         className="w-full h-full"
                         src={getAvatarLink(String(user.user_metadata.user_name))}
                       />
                     </div>
-
-                    <div className="flex-col shrink">
-                      <p className="text-osGrey text-xs font-semibold">
-                        {user.user_metadata.full_name}
-                      </p>
-
-                      <p className="text-gray-500 text-xs font-normal">
-                        {user.user_metadata.user_name}
-                      </p>
-                    </div>
                   </div>
-                </Menu.Item>
 
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      className={`${
-                        active ? "bg-gray-100 text-gray-700" : "text-gray-900"
-                      } group flex w-full items-center rounded-md px-5 py-1.5 text-sm`}
-                    >
-                      {`v${version}`}
-                    </button>
-                  )}
-                </Menu.Item>
+                  <div className="flex md:hidden w-5 h-5">
+                    <GiHamburgerMenu size={24} />
+                  </div>
+                </div>
+              </Menu.Button>
 
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      className={`${
-                        active ? "bg-gray-100 text-gray-700" : "text-gray-900"
-                      } group flex w-full items-center rounded-md px-5 py-1.5 text-sm`}
-                      onClick={() => handleFormOpen(true)}
-                    >
-                      Submit a repository
-                    </button>
-                  )}
-                </Menu.Item>
+              <Transition
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="z-40 absolute right-0 top-10 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <Menu.Item>
+                    <div className="flex items-center px-2 py-2.5 mb-1 gap-x-2.5">
+                      <div className="flex-col shrink-0 grow-0 w-8 h-8 overflow-hidden rounded-full border-osOrange border">
+                        <img
+                          alt={String(user.user_metadata.user_name)}
+                          className="w-full h-full"
+                          src={getAvatarLink(String(user.user_metadata.user_name))}
+                        />
+                      </div>
 
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      className={`${
-                        active ? "bg-gray-100 text-gray-700" : "text-gray-900"
-                      } group flex w-full items-center rounded-md px-5 py-1.5 text-sm`}
-                      onClick={() => console.log("Token: ", currentUser?.access_token)}
-                    >
-                      Print auth token
-                    </button>
-                  )}
-                </Menu.Item>
+                      <div className="flex-col shrink">
+                        <p className="text-osGrey text-xs font-semibold">
+                          {user.user_metadata.full_name}
+                        </p>
 
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href={bugReportLink}
-                      rel="noreferrer"
-                      target="_blank"
-                      className={`${
-                        active ? "bg-gray-100 text-gray-700" : "text-gray-900"
-                      } group flex w-full items-center rounded-md px-5 py-1.5 text-sm`}
-                    >
-                      Report a bug
-                    </a>
-                  )}
-                </Menu.Item>
+                        <p className="text-gray-500 text-xs font-normal">
+                          {user.user_metadata.user_name}
+                        </p>
+                      </div>
+                    </div>
+                  </Menu.Item>
 
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      className={`${
-                        active ? "bg-gray-100 text-gray-700" : "text-gray-900"
-                      } group flex w-full items-center rounded-md px-5 py-1.5 text-sm`}
-                      onClick={async () => signOut()}
-                    >
-                      Logout
-                    </button>
-                  )}
-                </Menu.Item>
-              </Menu.Items>
-            </Transition>
-          </Menu>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-gray-100 text-gray-700" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-5 py-1.5 text-sm`}
+                      >
+                        {`v${version}`}
+                      </button>
+                    )}
+                  </Menu.Item>
+
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-gray-100 text-gray-700" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-5 py-1.5 text-sm`}
+                        onClick={() => handleFormOpen(true)}
+                      >
+                        Submit a repository
+                      </button>
+                    )}
+                  </Menu.Item>
+
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-gray-100 text-gray-700" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-5 py-1.5 text-sm`}
+                        onClick={() => console.log("Token: ", currentUser?.access_token)}
+                      >
+                        Print auth token
+                      </button>
+                    )}
+                  </Menu.Item>
+
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href={bugReportLink}
+                        rel="noreferrer"
+                        target="_blank"
+                        className={`${
+                          active ? "bg-gray-100 text-gray-700" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-5 py-1.5 text-sm`}
+                      >
+                        Report a bug
+                      </a>
+                    )}
+                  </Menu.Item>
+
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-gray-100 text-gray-700" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-5 py-1.5 text-sm`}
+                        onClick={async () => signOut()}
+                      >
+                        Logout
+                      </button>
+                    )}
+                  </Menu.Item>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          </div>
         )}
 
         {!user && (
